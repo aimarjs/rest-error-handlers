bbe983429ea88e080fc0584a84de111ed80b0db2
Aimar Aareleid <aimar@snapit.ee>
2022-02-04 17:29:47 +0100

    feat: add multer error handling

--- src/middlewares/error_handler.ts
+++ src/middlewares/error_handler.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import multer from 'multer';
 import { CustomError } from '../errors/custom_error';
 
 export const errorHandler = (
@@ -8,6 +9,10 @@ export const errorHandler = (
 ) => {
     if (err instanceof CustomError) {
         return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+    }
+
+    if (err instanceof multer.MulterError) {
+        return res.status(+err.code).send({ errors: err.name, field: err.message });
     }
 
     res.status(400).send({
